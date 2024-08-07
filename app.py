3e1556590f67b9707fff12669d95636b25fea99c
Stephane ASSOCLE <mensste30@gmail.com>
2024-08-07 16:26:48 +0100

    user model with data

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "My Super Secret Key"
+#Config database for Mysql
+## Échappez le caractère @ dans le mot de passe
+password = 'stephaneP%40ssw0rd'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://stephane:{password}@localhost/flask_api"
+
+#init database
+db = SQLAlchemy(app)
 
 from controllers import *
 @app.route("/")
--- models/userModel.py
+++ models/userModel.py
@@ -1,3 +1,30 @@
-class User():
+from app import db
+from datetime import datetime
+from werkzeug.security import check_password_hash, generate_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(40), nullable=False)
+    role = db.Column(db.String(20), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    @property
+    def password(self):
+        raise AttributeError("Password is not a readable attribute")
+    
+    @password.setter
+    def password(self, pwd):
+        self.password_hash = generate_password_hash(pwd)
+
+    def verify_password(self, pwd)
+        return check_password_hash(self.password_hash, pwd)
+    
+    # Create a String
+    def __repr__(self):
+        return '<Name %r>' % self.name
+    
     def getName(self):
         return "My name is User"
