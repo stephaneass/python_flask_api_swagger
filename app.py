35b314c7fc4fada5d289d4531c5900a14bafe183
Stephane ASSOCLE <mensste30@gmail.com>
2024-08-07 17:37:06 +0100

    user seeder

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_seeder import FlaskSeeder
 
 app = Flask(__name__)
 
@@ -7,12 +8,17 @@ app.config['SECRET_KEY'] = "My Super Secret Key"
 #Config database for Mysql
 ## Échappez le caractère @ dans le mot de passe
 password = 'stephaneP%40ssw0rd'
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://stephane:{password}@localhost/flask_api"
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://stephane:{password}@localhost/flask_api"
 
 #init database
 db = SQLAlchemy(app)
+
+seeder = FlaskSeeder(app, db)
 
 from controllers import *
 @app.route("/")
 def index():
     return "You're on the index page"
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ seeds/userSeeder.py
@@ -0,0 +1,16 @@
+from flask_seeder import Seeder
+from models.userModel import Users
+from app import db
+
+class UserSeeder(Seeder):
+    def run(self):
+        
+        user1 = Users(id=1, name="Dieu est grand", email='dieu@gmail.com', password="1111", phone="67710659", role='super_admin')
+        user2 = Users(id=2, name="ASSOCLE Bright", email='bright@gmail.com', password="1111", phone="67710660", role='admin')
+        user3 = Users(id=3, name="ASSOCLE Prisca", email='prisca@gmail.com', password="1111", phone="67710661", role='user')
+
+        users_list = [ user1, user2, user3 ]
+
+        for user in users_list:
+            db.session.add(user)
+            db.session.commit()
